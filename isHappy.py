3e13887a665f7f80857bf6f790d0d90b3ad185c5
Emily Ding <xiaodidingv5@gmail.com>
2020-07-31 17:33:57 -0700

    algo practice

---
+++ isHappy.py
@@ -0,0 +1,16 @@
+def isHappy(n):
+    seen = set()
+    while n!= 1:
+        cur = 0
+        while n!= 0:
+            cur += (n%10)**2
+            n //=10
+        if cur in seen:
+            return False
+        seen.add(cur)
+        n = cur
+    return True
+
+print('hello')
+print(isHappy(200))
+
