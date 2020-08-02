0f071d2adc4c25c68decbd2f95b6db8a43b90a44
Emily Ding <xiaodidingv5@gmail.com>
2020-08-01 23:18:16 -0700

    algo practice

---
+++ detectCapital.py
@@ -0,0 +1,7 @@
+def detectCapitalUse(word):
+    if word.isupper() or word.islower():
+        return True
+    elif word[0].isupper() and word[1:].islower():
+        return True
+    else:
+        return False
